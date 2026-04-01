5f3d50329a54b14716b80c62f5194a8deda4ba94
Anshikachopra <anshikachopra246@gmail.com>
2026-04-01 08:30:30 +0530

    Add files via upload

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+android {
+    namespace = "com.example.zivara"
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+    defaultConfig {
+        applicationId = "com.example.zivara"
+        minSdk = flutter.minSdkVersion
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+flutter {
+    source = "../.."
+}
